0d2f22c224a10b71023117c42743d9a130dad5d9
Maryan Mayher <mr.maykher@gmail.com>
2016-04-29 22:40:26 +0300

    Update project with a base student views

--- eQuiz/modules/eQuiz.Web/Areas/Student/Controllers/DefaultController.cs
+++ eQuiz/modules/eQuiz.Web/Areas/Student/Controllers/DefaultController.cs
@@ -13,5 +13,20 @@ namespace eQuiz.Web.Areas.Student.Controllers
         {
             return View();
         }
+
+        public ActionResult Details()
+        {
+            return View();
+        }
+
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        public ActionResult Dashboard()
+        {
+            return View();
+        }
     }
 }
